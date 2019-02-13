dab9ca7cf50633db787a9d135f52f7041b12b777
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-02-13 11:50:22 -0200

    Improve view tests

--- core/views.py
+++ core/views.py
@@ -4,6 +4,17 @@ from .rank import localized_rank, missing_rank
 from .forms import SearchForm
 
 
+def _prepare_result(result, n_results=10):
+    return result.head(n_results).itertuples(index=False)
+
+
+def _prepare_person_attrs(person):
+    return zip(person.index, person)
+
+def _columns(result):
+    return list(result.columns)
+
+
 class HomeView(FormView):
     form_class = SearchForm
     template_name = 'core/home.html'
@@ -17,9 +28,13 @@ class SearchView(TemplateView):
         if form.is_valid():
             cleaned_data = form.cleaned_data
             if cleaned_data['search_type'] == '1':
-                result = localized_rank(cleaned_data['search_id'])
+                person, result = localized_rank(cleaned_data['search_id'])
             else:
-                result = missing_rank(cleaned_data['search_id'])
+                person, result = missing_rank(cleaned_data['search_id'])
 
-            context = {'result': result}
+            context = {
+                'result': _prepare_result(result),
+                'person_attrs': _prepare_person_attrs(person),
+                'column_names': _columns(result)
+            }
             return self.render_to_response(context)
