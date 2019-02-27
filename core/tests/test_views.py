92b8b9f4e7b34c0caf303841b800444c50a97418
Rhenan Bartels <rhenan.bartels@gmail.com>
2019-02-27 12:21:02 -0300

    Remove search_type from querystring and use and valid id

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -18,17 +18,18 @@ class ViewsTest(TestCase):
     @mock.patch('core.views._prepare_person_attrs')
     @mock.patch('core.views.localized_rank')
     def test_search_missing_by_id(self, _loc_rank, _attrs, _result, _cols):
+        # search_id must contains DS in it
         _result.return_value = 'result'
         _attrs.return_value = [('a', 1), ('b', 2)]
         _cols.return_value = 'col names'
 
         _loc_rank.return_value = ['person', 'df']
-        url = reverse('core:search') + '?search_id=1234&search_type=1'
+        url = reverse('core:search') + '?search_id=1234DS45678'
 
         resp = self.client.get(url)
 
         self.assertEqual(resp.status_code, 200)
-        _loc_rank.assert_called_once_with('1234')
+        _loc_rank.assert_called_once_with('1234DS45678')
         self.assertEqual(resp.context['result'], 'result')
         self.assertEqual(resp.context['person_attrs'], [('a', 1), ('b', 2)])
         self.assertEqual(resp.context['column_names'], 'col names')
