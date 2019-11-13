6b5118b780f505ae7a1631fa1030473c5505f320
rhenanbartels <rhenan.bartels@gmail.com>
2019-11-13 11:55:18 -0300

    Remove unused functions

--- core/tests/test_rank.py
+++ core/tests/test_rank.py
@@ -1,58 +1,10 @@
 from datetime import datetime as dt
 from decimal import Decimal
-from unittest import TestCase, mock
+from unittest import TestCase
 
 import pandas
 
-from core.rank import (
-    missing_rank,
-    localized_rank,
-    search_type,
-    lat_long_score)
-
-
-class Rank(TestCase):
-    @mock.patch('core.rank.search_single_missing')
-    def test_missing_not_found(self, _missing):
-        _missing.return_value = None
-
-        rank, person = missing_rank(localized_id='1234')
-
-        self.assertTrue(rank is None)
-        self.assertTrue(person is None)
-
-    @mock.patch('core.rank.search_single_localized')
-    def test_localized_not_found(self, _localized):
-        _localized.return_value = None
-
-        rank, person = localized_rank(missing_id='1234')
-
-        self.assertTrue(rank is None)
-        self.assertTrue(person is None)
-
-
-class AutoSearchType(TestCase):
-    def test_find_search_type_based_on_id_search_type_one(self):
-        """
-            If DS in id it is a missing person
-        """
-        _id = '1234RJDS98765'
-
-        st = search_type(_id)
-        expected = 1
-
-        self.assertEqual(st, expected)
-
-    def test_find_search_type_based_on_id_search_type_two(self):
-        """
-            If DS in id it is a missing person
-        """
-        _id = '1234RJ98765'
-
-        st = search_type(_id)
-        expected = 2
-
-        self.assertEqual(st, expected)
+from core.rank import lat_long_score
 
 
 class LatLongScore(TestCase):
@@ -109,6 +61,89 @@ class LatLongScore(TestCase):
                 'CIDADE 1',
                 Decimal('-22.8658255011035'),
                 Decimal('-70.2539217453901'),
+                '12345'
+            ),
+            (
+                dt(2017, 2, 2, 0, 0),
+                None,
+                None,
+                None,
+                Decimal('-22.8658255011035'),
+                Decimal('-51.2539217453901'),
+                'BAIRRO 2',
+                'CIDADE 2',
+                Decimal('-22.8658255011035'),
+                Decimal('-51.2539217453901'),
+                '67890'
+            )
+        ]
+        all_persons_df = pandas.DataFrame(
+            all_person_data,
+            columns=[
+                'data_fato',
+                'cidade_latitude',
+                'cidade_longitude',
+                'cidade_nome',
+                'bairro_latitude',
+                'bairro_longitude',
+                'bairro_nome',
+                'cidade_bairro',
+                'cidade_bairro_latittude',
+                'cidade_bairro_longitude',
+                'id_sinalid'
+            ]
+        )
+
+        score_df = lat_long_score(target_df, all_persons_df)
+
+        expected = all_persons_df.copy()
+        expected.loc[0, 'lat_long_score'] = 0.000573516963808812
+        expected.loc[1, 'lat_long_score'] = 0.004872211615539773
+
+        pandas.testing.assert_frame_equal(score_df, expected)
+
+    def test_target_has_bairro_one_row_has_no_bairro(self):
+        target_data = (
+            dt(2017, 2, 2, 0, 0),
+            None,
+            None,
+            None,
+            Decimal('-22.8658255011035'),
+            Decimal('-53.2539217453901'),
+            'BAIRRO',
+            'CIDADE',
+            Decimal('-22.8658255011035'),
+            Decimal('-53.2539217453901'),
+            '12345'
+        )
+        target_df = pandas.Series(
+            target_data,
+            index=[
+                'data_fato',
+                'cidade_latitude',
+                'cidade_longitude',
+                'cidade_nome',
+                'bairro_latitude',
+                'bairro_longitude',
+                'bairro_nome',
+                'cidade_bairro',
+                'cidade_bairro_latittude',
+                'cidade_bairro_longitude',
+                'id_sinalid'
+            ]
+        )
+        all_person_data = [
+            (
+                dt(2015, 2, 2, 0, 0),
+                Decimal('-22.8658255011035'),
+                Decimal('-70.2539217453901'),
+                'Cidade 1',
+                None,
+                None,
+                None,
+                None,
+                None,
+                None,
                 '12345'
             ),
             (
--- core/views.py
+++ core/views.py
@@ -1,9 +1,7 @@
-from django.contrib import messages
 from django.shortcuts import redirect
 from django.views.generic import FormView, TemplateView
 
-from .rank import localized_rank, missing_rank, search_type
-from .forms import SearchForm
+from core.forms import SearchForm
 
 
 def _prepare_result(result, n_results=10):
@@ -27,30 +25,4 @@ class SearchView(TemplateView):
     template_name = 'core/search.html'
 
     def get(self, request, *args, **kwargs):
-        form = SearchForm(request.GET)
-        context = {'form': SearchForm()}
-        if form.is_valid():
-            cleaned_data = form.cleaned_data
-            search_id = cleaned_data['search_id']
-            _type = search_type(search_id)
-            if _type == 1:
-                person, result = localized_rank(search_id)
-            else:
-                person, result = missing_rank(search_id)
-
-            if person is None:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    'Identificador Sinalid não encontrado'
-                )
-                return self.render_to_response(context)
-            else:
-                context['result'] = _prepare_result(result)
-                context['person_attrs'] = _prepare_person_attrs(person)
-                context['column_names'] = _columns(result)
-                context['search_type'] = _type
-                return self.render_to_response(context)
-
-        else:
-            return redirect('core:home')
+        return redirect('core:home')
